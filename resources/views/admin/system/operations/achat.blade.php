79f736c522087d0e16f14d5b25a734dbdaaef5f2
glinux99 <interlab21@yahoo.com>
2022-11-07 11:22:46 +0100

    e-commerce

--- resources/views/admin/system/operations/achat.blade.php
+++ resources/views/admin/system/operations/achat.blade.php
@@ -5,77 +5,74 @@
 
 
         <div class="aiz-titlebar text-left mt-2 mb-3">
-            <h5 class="mb-0 h6">@lang("Ajouter une nouvelle matiere premiere")</h5>
+            <h5 class="mb-0 h6">@lang("Ajouter une nouvelle Operation")</h5>
         </div>
         <div class="">
-            <form class="form form-horizontal mar-top" action="{{ route('produit.store')}}" method="POST" enctype="multipart/form-data" id="choice_form">
+            <form class="form form-horizontal mar-top" action="#" method="POST" enctype="multipart/form-data" id="choice_form">
                 @csrf
                 <div class="row gutters-5">
-                    <div class="col-lg-12">
+                    <div class="col-lg-8">
                         <div class="card">
                             <div class="card-header">
-                                <h5 class="mb-0 h6">@lang("Information sur la matiere premiere")</h5>
+                                <h5 class="mb-0 h6">@lang("Information sur l'Operation d'Achat")</h5>
                             </div>
                             <div class="card-body">
                                 <div class="form-group row">
-                                    <label class="col-md-3 col-from-label">@lang("Designation") <span class="text-danger">*</span></label>
+                                    <label class="col-md-3 col-from-label">@lang("Article") <span class="text-danger">*</span></label>
                                     <div class="col-md-8">
                                         <input type="text" class="form-control" name="name" placeholder="@lang(' Nom du produit')" onchange="update_sku()" required>
                                     </div>
                                 </div>
                                 <div class="form-group row" id="brand">
-                                    <label class="col-md-3 col-from-label">@lang("Marque")</label>
+                                    <label class="col-md-3 col-from-label">@lang("Prix")</label>
                                     <div class="col-md-8">
-                                        <select class="form-control selectcategorie" name="categorie" id="brand_id" data-live-search="true">
-                                        </select>
+                                        <input type="number" name="" id="" class="form-control">
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label class="col-md-3 col-from-label">@lang("Type d'embalage")</label>
+                                    <label class="col-md-3 col-from-label">@lang("Date")</label>
                                     <div class="col-md-8">
-                                        <input type="text" class="form-control" name="unite" placeholder="Unit (e.g. KG, Pc etc)" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-md-3 col-from-label">@lang("Unite par defaut")</label>
-                                    <div class="col-md-8">
-                                        <input type="text" class="form-control" name="unite" placeholder="Unit (e.g. KG, Pc etc)" required>
-                                    </div>
-                                </div>
-                                @yield('compte')
-                                <div class="form-group row">
-                                    <label class="col-md-3 col-from-label">@lang("Sous compte") <span class="text-danger">*</span></label>
-                                    <div class="col-md-8">
-                                        <input type="text" class="form-control" name="name" placeholder="@lang(' Nom du produit')" onchange="update_sku()" required>
+                                        <input type="date" class="form-control">
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="card">
                             <div class="card-header">
-                                <h5 class="mb-0 h6">@lang("Description de la matiere premiere")</h5>
+                                <h5 class="mb-0 h6">@lang("Detail des articles pour l'achat")</h5>
                             </div>
                             <div class="card-body">
                                 <div class="form-group row">
-                                    <label class="col-md-3 col-from-label">@lang("La description")</label>
+                                    <label class="col-md-3 col-from-label">@lang("Libele")</label>
                                     <div class="col-md-8">
-                                        <textarea class="aiz-text-editor" name="description"></textarea>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-from-label">@lang("Montant Total")</label>
+                                    <div class="col-md-8">
+                                        <input type="number" class="form-control" value="12" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-from-label">@lang("Montant de la TVA")</label>
+                                    <div class="col-md-8">
+                                        <input type="number" class="form-control" value="0">
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-                        <!--                <div class="card">
-                    <div class="card-header">
-                        <h5 class="mb-0 h6">Product Shipping Cost</h5>
                     </div>
-                    <div class="card-body">
-
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="mb-0 h6">@lang("Liste des articles achetes")</h5>
+                            </div>
+                            <div class="card-body">
+                                LIste Here
+                            </div>
+                        </div>
                     </div>
-                </div>-->
-
-                    </div>
-
                     <div class="col-12">
                         <div class="btn-toolbar float-right mb-3" role="toolbar" aria-label="Toolbar with button groups">
                             <div class="btn-group mr-2" role="group" aria-label="Third group">
